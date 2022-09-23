51691459f3778cb0b83c7777e00a6dcdeed60d57
carla-ng <carlitashata@gmail.com>
2022-09-23 21:28:49 +0200

    Set and get computed properties

--- src/composables/useUI.js
+++ src/composables/useUI.js
@@ -9,7 +9,15 @@ const useUI = () => {
     return {
 
         // Side menu options
-        sideMenuOpen: computed( () => store.getters['ui/isSideMenuOpen'] ),
+        //sideMenuOpen: computed( () => store.getters['ui/isSideMenuOpen'] ),
+        sideMenuOpen: computed({
+            get() {
+                return store.getters['ui/isSideMenuOpen']
+            },
+            set(val) {
+                store.commit('ui/toggleSideMenu')
+            }
+        }),
         toggleSideMenu() { store.commit('ui/toggleSideMenu') },
 
     }
